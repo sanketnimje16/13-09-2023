a4268e28074e00d03960543f36d8bebd6d862b4b
sanketnimje16 <snimjesanket786@gmail.com>
2023-10-26 14:09:03 +0530

    JavaScript Function

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*console.log("Hello from external file");
+let p = 49;
+let q = 7;
+
+// lets create a function
+function divide(parameter1,parameter2){
+    // Every function return something
+    // function body 
+    // return is a result
+    return parameter1 / parameter2;
+
+}
+console.log(divide(63,9));
+*/
---
+++ js/script1.js
@@ -0,0 +1,17 @@
+// let n = 10;
+// let m = 50;
+// // lets create a function
+// function multiply(multi1,multi2){
+//     //return is a result
+//     return multi1 * multi2 ;
+// }
+// console.log(multiply(10,5));
+// //Always remember you can receive String with Prompt
+// let number1 = window.prompt("enter the first value");
+// // lets convert string to Integer with the help of type conversion/type casting
+// // To change one data types to another Data types is called Type casting/conversion
+// number1 = parseFloat(number1);
+// let number2 = window.prompt("enter the second value")
+// number2 = parseFloat(number2);
+// // lets create a function
+// alert(number1 * number2);
