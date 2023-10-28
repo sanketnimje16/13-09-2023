e5e12212cbf344a26923020ab569a1c4acaf239f
sanketnimje16 <snimjesanket786@gmail.com>
2023-10-28 10:19:57 +0530

    comments update

--- js/script1.js
+++ js/script1.js
@@ -9,7 +9,7 @@ console.log(multiply(10,5));
 //Always remember you can receive String with Prompt
 let number1 = window.prompt("enter the first value");
 // lets convert string to Integer with the help of type conversion/type casting
-// To change one data types to another Data types is called Type casting/conversion
+// To change one data types converts into another Data types is called Type casting/conversion
 number1 = parseFloat(number1);
 let number2 = window.prompt("enter the second value")
 number2 = parseFloat(number2);
